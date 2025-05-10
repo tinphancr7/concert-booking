aa75e16c4510709d80aca1a5544dd0b627e26d4c
Tin Phan <phanthanhtincr7@gmail.com>
2025-05-10 07:00:04 +0700

    up

--- src/schemas/course.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod'
-
-export const createCourseSchema = z.object({
-  title: z.string(),
-  slug: z.string()
-})
--- src/types/video.type.ts
+++
@@ -1,6 +0,0 @@
-import { Document, Schema } from 'mongoose'
-
-export interface Video extends Document {
-  video_url: string
-  content: string
-}
